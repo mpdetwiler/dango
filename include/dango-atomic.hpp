6b84d1eeb63908dd8285bca792b8d551804cf467
mpdetwiler <mpdetwiler@gmail.com>
2019-01-05 14:15:12 -0500

    atomic

--- include/dango-atomic.hpp
+++ include/dango-atomic.hpp
@@ -85,62 +85,64 @@ atomic<tp_type, DANGO_ATOMIC_INT_ENABLE_SPEC(tp_type)>
 final
 {
 public:
+  using value_type = tp_type;
+
   static constexpr auto is_lock_free()noexcept->bool;
 public:
-  explicit constexpr atomic(tp_type)noexcept;
+  explicit constexpr atomic(value_type)noexcept;
 
   ~atomic()noexcept = default;
 
   template
   <dango::mem_order tp_order = dango::mem_order::seq_cst>
-  auto load()const noexcept->tp_type;
+  auto load()const noexcept->value_type;
   template
   <dango::mem_order tp_order = dango::mem_order::seq_cst>
-  void store(tp_type)noexcept;
+  void store(value_type)noexcept;
   template
   <dango::mem_order tp_order = dango::mem_order::seq_cst>
-  auto exchange(tp_type)noexcept->tp_type;
+  auto exchange(value_type)noexcept->value_type;
 
   template
   <
     dango::mem_order tp_success = dango::mem_order::seq_cst,
     dango::mem_order tp_failure = dango::mem_order::seq_cst
   >
-  auto compare_exchange(tp_type&, tp_type)noexcept->bool;
+  auto compare_exchange(value_type&, value_type)noexcept->bool;
 
   template
   <dango::mem_order tp_order = dango::mem_order::seq_cst>
-  auto fetch_add(tp_type)noexcept->tp_type;
+  auto fetch_add(value_type)noexcept->value_type;
   template
   <dango::mem_order tp_order = dango::mem_order::seq_cst>
-  auto fetch_sub(tp_type)noexcept->tp_type;
+  auto fetch_sub(value_type)noexcept->value_type;
   template
   <dango::mem_order tp_order = dango::mem_order::seq_cst>
-  auto fetch_and(tp_type)noexcept->tp_type;
+  auto fetch_and(value_type)noexcept->value_type;
   template
   <dango::mem_order tp_order = dango::mem_order::seq_cst>
-  auto fetch_or(tp_type)noexcept->tp_type;
+  auto fetch_or(value_type)noexcept->value_type;
   template
   <dango::mem_order tp_order = dango::mem_order::seq_cst>
-  auto fetch_xor(tp_type)noexcept->tp_type;
+  auto fetch_xor(value_type)noexcept->value_type;
 
   template
   <dango::mem_order tp_order = dango::mem_order::seq_cst>
-  auto add_fetch(tp_type)noexcept->tp_type;
+  auto add_fetch(value_type)noexcept->value_type;
   template
   <dango::mem_order tp_order = dango::mem_order::seq_cst>
-  auto sub_fetch(tp_type)noexcept->tp_type;
+  auto sub_fetch(value_type)noexcept->value_type;
   template
   <dango::mem_order tp_order = dango::mem_order::seq_cst>
-  auto and_fetch(tp_type)noexcept->tp_type;
+  auto and_fetch(value_type)noexcept->value_type;
   template
   <dango::mem_order tp_order = dango::mem_order::seq_cst>
-  auto or_fetch(tp_type)noexcept->tp_type;
+  auto or_fetch(value_type)noexcept->value_type;
   template
   <dango::mem_order tp_order = dango::mem_order::seq_cst>
-  auto xor_fetch(tp_type)noexcept->tp_type;
+  auto xor_fetch(value_type)noexcept->value_type;
 private:
-  tp_type m_data;
+  value_type m_data;
 public:
   DANGO_DELETE_DEFAULT_CTOR(atomic)
   DANGO_IMMOBILE(atomic)
@@ -154,9 +156,9 @@ atomic<tp_type, DANGO_ATOMIC_INT_ENABLE_SPEC(tp_type)>::
 is_lock_free
 ()noexcept->bool
 {
-  auto const a_temp = tp_type(0);
+  auto a_temp = value_type(0);
 
-  return __atomic_always_lock_free(sizeof(tp_type), &a_temp);
+  return __atomic_always_lock_free(sizeof(value_type), &a_temp);
 }
 
 template
@@ -165,7 +167,7 @@ constexpr
 dango::
 atomic<tp_type, DANGO_ATOMIC_INT_ENABLE_SPEC(tp_type)>::
 atomic
-(tp_type const a_data)noexcept:
+(value_type const a_data)noexcept:
 m_data{ a_data }
 {
 
@@ -179,7 +181,7 @@ auto
 dango::
 atomic<tp_type, DANGO_ATOMIC_INT_ENABLE_SPEC(tp_type)>::
 load
-()const noexcept->tp_type
+()const noexcept->value_type
 {
   static_assert(detail::is_valid_mem_order(tp_order));
 
@@ -197,11 +199,11 @@ template
 <typename tp_type>
 template
 <dango::mem_order tp_order>
-auto
+void
 dango::
 atomic<tp_type, DANGO_ATOMIC_INT_ENABLE_SPEC(tp_type)>::
 store
-(tp_type const a_data)noexcept->void
+(value_type const a_data)noexcept
 {
   static_assert(detail::is_valid_mem_order(tp_order));
 
@@ -223,7 +225,7 @@ auto
 dango::
 atomic<tp_type, DANGO_ATOMIC_INT_ENABLE_SPEC(tp_type)>::
 exchange
-(tp_type const a_data)noexcept->tp_type
+(value_type const a_data)noexcept->value_type
 {
   static_assert(detail::is_valid_mem_order(tp_order));
 
@@ -241,7 +243,7 @@ auto
 dango::
 atomic<tp_type, DANGO_ATOMIC_INT_ENABLE_SPEC(tp_type)>::
 compare_exchange
-(tp_type& a_expected, tp_type const a_data)noexcept->bool
+(value_type& a_expected, value_type const a_data)noexcept->bool
 {
   static_assert(detail::is_valid_mem_order(tp_success));
   static_assert(detail::is_valid_mem_order(tp_failure));
@@ -267,7 +269,7 @@ compare_exchange
   return a_result;
 }
 
-#define DANGO_ATOMIC_DEFINE_FETCH_OP(oper)                                  \
+#define DANGO_ATOMIC_DEFINE_FETCH(name)                                     \
 template                                                                    \
 <typename tp_type>                                                          \
 template                                                                    \
@@ -275,45 +277,27 @@ template                                                                    \
 auto                                                                        \
 dango::                                                                     \
 atomic<tp_type, DANGO_ATOMIC_INT_ENABLE_SPEC(tp_type)>::                    \
-fetch_##oper                                                                \
-(tp_type const a_data)noexcept->tp_type                                     \
+name                                                                        \
+(value_type const a_data)noexcept->value_type                               \
 {                                                                           \
   static_assert(detail::is_valid_mem_order(tp_order));                      \
                                                                             \
-  return __atomic_fetch_##oper(&m_data, a_data, dango::s_int(tp_order));    \
+  return __atomic_##name(&m_data, a_data, dango::s_int(tp_order));          \
 }
 
-DANGO_ATOMIC_DEFINE_FETCH_OP(add)
-DANGO_ATOMIC_DEFINE_FETCH_OP(sub)
-DANGO_ATOMIC_DEFINE_FETCH_OP(and)
-DANGO_ATOMIC_DEFINE_FETCH_OP(or)
-DANGO_ATOMIC_DEFINE_FETCH_OP(xor)
+DANGO_ATOMIC_DEFINE_FETCH(fetch_add)
+DANGO_ATOMIC_DEFINE_FETCH(fetch_sub)
+DANGO_ATOMIC_DEFINE_FETCH(fetch_and)
+DANGO_ATOMIC_DEFINE_FETCH(fetch_or)
+DANGO_ATOMIC_DEFINE_FETCH(fetch_xor)
 
-#undef DANGO_ATOMIC_DEFINE_FETCH_OP
+DANGO_ATOMIC_DEFINE_FETCH(add_fetch)
+DANGO_ATOMIC_DEFINE_FETCH(sub_fetch)
+DANGO_ATOMIC_DEFINE_FETCH(and_fetch)
+DANGO_ATOMIC_DEFINE_FETCH(or_fetch)
+DANGO_ATOMIC_DEFINE_FETCH(xor_fetch)
 
-#define DANGO_ATOMIC_DEFINE_OP_FETCH(oper)                                  \
-template                                                                    \
-<typename tp_type>                                                          \
-template                                                                    \
-<dango::mem_order tp_order>                                                 \
-auto                                                                        \
-dango::                                                                     \
-atomic<tp_type, DANGO_ATOMIC_INT_ENABLE_SPEC(tp_type)>::                    \
-oper##_fetch                                                                \
-(tp_type const a_data)noexcept->tp_type                                     \
-{                                                                           \
-  static_assert(detail::is_valid_mem_order(tp_order));                      \
-                                                                            \
-  return __atomic_##oper##_fetch(&m_data, a_data, dango::s_int(tp_order));  \
-}
-
-DANGO_ATOMIC_DEFINE_OP_FETCH(add)
-DANGO_ATOMIC_DEFINE_OP_FETCH(sub)
-DANGO_ATOMIC_DEFINE_OP_FETCH(and)
-DANGO_ATOMIC_DEFINE_OP_FETCH(or)
-DANGO_ATOMIC_DEFINE_OP_FETCH(xor)
-
-#undef DANGO_ATOMIC_DEFINE_OP_FETCH
+#undef DANGO_ATOMIC_DEFINE_FETCH
 
 #undef DANGO_ATOMIC_INT_ENABLE_SPEC
 
@@ -326,8 +310,186 @@ dango::
 atomic<tp_type*, dango::enable_tag>
 final
 {
+public:
+  using value_type = tp_type*;
 
+  static constexpr auto is_lock_free()noexcept->bool;
+public:
+  explicit constexpr atomic(value_type)noexcept;
+
+  ~atomic()noexcept = default;
+
+  template
+  <dango::mem_order tp_order = dango::mem_order::seq_cst>
+  auto load()const noexcept->value_type;
+  template
+  <dango::mem_order tp_order = dango::mem_order::seq_cst>
+  void store(value_type)noexcept;
+  template
+  <dango::mem_order tp_order = dango::mem_order::seq_cst>
+  auto exchange(value_type)noexcept->value_type;
+
+  template
+  <
+    dango::mem_order tp_success = dango::mem_order::seq_cst,
+    dango::mem_order tp_failure = dango::mem_order::seq_cst
+  >
+  auto compare_exchange(value_type&, value_type)noexcept->bool;
+
+  template
+  <
+    dango::mem_order tp_order = dango::mem_order::seq_cst,
+    dango::enable_if<!dango::is_func<tp_type>> = dango::enable_val
+  >
+  auto fetch_add(dango::ssize)noexcept->value_type;
+
+  template
+  <
+    dango::mem_order tp_order = dango::mem_order::seq_cst,
+    dango::enable_if<!dango::is_func<tp_type>> = dango::enable_val
+  >
+  auto fetch_sub(dango::ssize)noexcept->value_type;
+
+  template
+  <
+    dango::mem_order tp_order = dango::mem_order::seq_cst,
+    dango::enable_if<!dango::is_func<tp_type>> = dango::enable_val
+  >
+  auto add_fetch(dango::ssize)noexcept->value_type;
+
+  template
+  <
+    dango::mem_order tp_order = dango::mem_order::seq_cst,
+    dango::enable_if<!dango::is_func<tp_type>> = dango::enable_val
+  >
+  auto sub_fetch(dango::ssize)noexcept->value_type;
+private:
+  value_type m_data;
+public:
+  DANGO_DELETE_DEFAULT_CTOR(atomic)
+  DANGO_IMMOBILE(atomic)
 };
+
+template
+<typename tp_type>
+constexpr auto
+dango::
+atomic<tp_type*, dango::enable_tag>::
+is_lock_free
+()noexcept->bool
+{
+  auto a_temp = nullptr;
+
+  return __atomic_always_lock_free(sizeof(value_type), &a_temp);
+}
+
+template
+<typename tp_type>
+constexpr
+dango::
+atomic<tp_type*, dango::enable_tag>::
+atomic
+(value_type const a_data)noexcept:
+m_data{ a_data }
+{
+
+}
+
+template
+<typename tp_type>
+template
+<dango::mem_order tp_order>
+auto
+dango::
+atomic<tp_type*, dango::enable_tag>::
+load
+()const noexcept->value_type
+{
+  static_assert(detail::is_valid_mem_order(tp_order));
+
+  static_assert
+  (
+    tp_order == dango::mem_order::relaxed ||
+    tp_order == dango::mem_order::acquire ||
+    tp_order == dango::mem_order::seq_cst
+  );
+
+  return __atomic_load_n(&m_data, dango::s_int(tp_order));
+}
+
+template
+<typename tp_type>
+template
+<dango::mem_order tp_order>
+void
+dango::
+atomic<tp_type*, dango::enable_tag>::
+store
+(value_type const a_data)noexcept
+{
+  static_assert(detail::is_valid_mem_order(tp_order));
+
+  static_assert
+  (
+    tp_order == dango::mem_order::relaxed ||
+    tp_order == dango::mem_order::release ||
+    tp_order == dango::mem_order::seq_cst
+  );
+
+  __atomic_store_n(&m_data, a_data, dango::s_int(tp_order));
+}
+
+template
+<typename tp_type>
+template
+<dango::mem_order tp_order>
+auto
+dango::
+atomic<tp_type*, dango::enable_tag>::
+exchange
+(value_type const a_data)noexcept->value_type
+{
+  static_assert(detail::is_valid_mem_order(tp_order));
+
+  return __atomic_exchange_n(&m_data, a_data, dango::s_int(tp_order));
+}
+
+template
+<typename tp_type>
+template
+<
+  dango::mem_order tp_success,
+  dango::mem_order tp_failure
+>
+auto
+dango::
+atomic<tp_type*, dango::enable_tag>::
+compare_exchange
+(value_type& a_expected, value_type const a_data)noexcept->bool
+{
+  static_assert(detail::is_valid_mem_order(tp_success));
+  static_assert(detail::is_valid_mem_order(tp_failure));
+
+  static_assert
+  (
+    tp_failure == dango::mem_order::relaxed ||
+    tp_failure == dango::mem_order::acquire ||
+    tp_failure == dango::mem_order::seq_cst
+  );
+
+  bool const a_result =
+  __atomic_compare_exchange_n
+  (
+    &m_data,
+    &a_expected,
+    a_data,
+    false,
+    dango::s_int(tp_success),
+    dango::s_int(tp_failure)
+  );
+
+  return a_result;
+}
 
 /*************************************************************/
 
