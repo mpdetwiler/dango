3101691c4de76084793c59088f74f33e535f1c78
mpdetwiler <mpdetwiler@gmail.com>
2019-01-12 00:10:53 -0500

    global

--- src/main.cpp
+++ src/main.cpp
@@ -77,6 +77,44 @@ accessor::
   printf("accessed\n");
 }
 
+#include <tuple>
+
+namespace
+dango::detail
+{
+  template
+  <typename tp_type>
+  struct parenth_type_help;
+
+  template
+  <typename tp_type>
+  struct parenth_type_help<void(tp_type)noexcept>;
+}
+
+namespace
+dango
+{
+  template
+  <typename tp_type>
+  using parenth_type =
+    typename detail::parenth_type_help<tp_type>::type;
+}
+
+template
+<typename tp_type>
+struct
+dango::
+detail::
+parenth_type_help<void(tp_type)noexcept>
+final
+{
+  using type = tp_type;
+};
+
+#define DANGO_COMMA_TYPE(...) dango::parenth_type<void(__VA_ARGS__)noexcept>
+
+DANGO_DEFINE_GLOBAL_INLINE(s_tuple, DANGO_COMMA_TYPE(std::tuple<int, bool>), 1, true)
+
 auto
 main
 ()noexcept->dango::s_int
