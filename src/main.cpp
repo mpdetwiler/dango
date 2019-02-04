44409fe943b13f70458b79efba7146836b697bd8
mpdetwiler <mpdetwiler@gmail.com>
2019-02-03 19:45:15 -0800

    concurrent

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,24 @@ auto
 main
 ()noexcept->dango::s_int
 {
+  fprintf(stderr, "tick_count: %llu\n", dango::u_cent(dango::get_tick_count()));
+  fprintf(stderr, "tick_count_sa: %llu\n", dango::u_cent(dango::get_tick_count_sa()));
+  fprintf(stderr, "sleep...\n");
 
+  dango::thread::sleep(30'000);
+
+  fprintf(stderr, "tick_count: %llu\n", dango::u_cent(dango::get_tick_count()));
+  fprintf(stderr, "tick_count_sa: %llu\n", dango::u_cent(dango::get_tick_count_sa()));
+  fprintf(stderr, "sleep_sa...\n");
+
+  dango::thread::sleep_sa(30'000);
+
+  for(auto a_i = uint32(0); a_i != uint32(40); ++a_i)
+  {
+    fprintf(stderr, "tick_count: %llu\n", dango::u_cent(dango::get_tick_count()));
+    fprintf(stderr, "tick_count_sa: %llu\n", dango::u_cent(dango::get_tick_count_sa()));
+    dango::thread::sleep(1);
+  }
 
   return 0;
 }
