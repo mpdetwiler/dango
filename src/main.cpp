6dbb4fa0c845642c6ade096d30330102db1b3977
mpdetwiler <mpdetwiler@gmail.com>
2019-02-03 20:38:20 -0800

    concurrent

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,56 @@ auto
 main
 ()noexcept->dango::s_int
 {
-  fprintf(stdout, "tick_count: %llu\n", dango::u_cent(dango::get_tick_count()));
-  fprintf(stdout, "tick_count_sa: %llu\n", dango::u_cent(dango::get_tick_count_sa()));
-  fprintf(stdout, "sleep...\n");
+  dango::mutex a_mutex{ };
+  dango::cond_var_mutex a_cond{ a_mutex };
 
-  dango::thread::sleep(30'000);
+  {
+    auto const a_deadline = dango::make_deadline_rel(30'000);
 
-  fprintf(stdout, "tick_count: %llu\n", dango::u_cent(dango::get_tick_count()));
-  fprintf(stdout, "tick_count_sa: %llu\n", dango::u_cent(dango::get_tick_count_sa()));
-  fprintf(stdout, "sleep_sa...\n");
+    fprintf(stdout, "tick_count: %llu\n", dango::u_cent(dango::get_tick_count()));
+    fprintf(stdout, "tick_count_sa: %llu\n", dango::u_cent(dango::get_tick_count_sa()));
+    fprintf(stdout, "sleep...\n");
 
-  dango::thread::sleep_sa(30'000);
+    dango_crit_full(a_cond, a_crit)
+    {
+      while(!a_deadline.has_passed())
+      {
+        a_crit.wait(a_deadline);
+      }
+    }
+  }
+
+  {
+    auto const a_deadline = dango::make_deadline_rel_sa(30'000);
+
+    fprintf(stdout, "tick_count: %llu\n", dango::u_cent(dango::get_tick_count()));
+    fprintf(stdout, "tick_count_sa: %llu\n", dango::u_cent(dango::get_tick_count_sa()));
+    fprintf(stdout, "sleep_sa...\n");
+
+    dango_crit_full(a_cond, a_crit)
+    {
+      while(!a_deadline.has_passed())
+      {
+        a_crit.wait(a_deadline);
+      }
+    }
+  }
+
+  auto a_deadline = dango::make_deadline_rel(5);
 
   for(auto a_i = uint32(0); a_i != uint32(40); ++a_i)
   {
     fprintf(stdout, "tick_count: %llu\n", dango::u_cent(dango::get_tick_count()));
-    dango::thread::sleep(5);
+
+    dango_crit_full(a_cond, a_crit)
+    {
+      while(!a_deadline.has_passed())
+      {
+        a_crit.wait(a_deadline);
+      }
+    }
+
+    a_deadline.add(5);
   }
 
   return 0;
