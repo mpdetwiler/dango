5b20c5469747a877bb506b3309b1438f2618ba27
Michael Detwiler <mpdetwiler@gmail.com>
2019-03-07 19:54:19 -0800

    tuple

--- include/dango-macro.hpp
+++ include/dango-macro.hpp
@@ -3,12 +3,12 @@
 
 #define DANGO_UNCOPYABLE(name) \
   constexpr name(name const&)noexcept = delete; \
-  constexpr auto operator = (name const&)&noexcept->name& = delete;
+  constexpr auto operator = (name const&)& noexcept->name& = delete;
 
 #define DANGO_IMMOBILE(name) \
   DANGO_UNCOPYABLE(name) \
   constexpr name(name&&)noexcept = delete; \
-  constexpr auto operator = (name&&)&noexcept->name& = delete;
+  constexpr auto operator = (name&&)& noexcept->name& = delete;
 
 #define DANGO_DELETE_DEFAULT(name) \
   constexpr name()noexcept = delete;
@@ -19,16 +19,16 @@
   ~name()noexcept = delete;
 
 #define DANGO_UNASSIGNABLE(name) \
-  constexpr auto operator = (name const&)&noexcept->name& = delete; \
-  constexpr auto operator = (name&&)&noexcept->name& = delete;
+  constexpr auto operator = (name const&)& noexcept->name& = delete; \
+  constexpr auto operator = (name&&)& noexcept->name& = delete;
 
 #define DANGO_TAG_TYPE(name) \
   constexpr name()noexcept = default; \
   constexpr name(name const&)noexcept = default; \
   constexpr name(name&&)noexcept = default; \
   ~name()noexcept = default; \
-  constexpr auto operator = (name const&)&noexcept->name& = default; \
-  constexpr auto operator = (name&&)&noexcept->name& = default;
+  constexpr auto operator = (name const&)& noexcept->name& = default; \
+  constexpr auto operator = (name&&)& noexcept->name& = default;
 
 #define DANGO_TOKEN_CONCAT_IMPL(x, y) x##y
 #define DANGO_TOKEN_CONCAT(x, y) DANGO_TOKEN_CONCAT_IMPL(x, y)
