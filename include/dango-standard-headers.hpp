1d99010e0dd95a09943051ca66099c6bb2a7a0ba
mpdetwiler <mpdetwiler@gmail.com>
2020-12-08 16:00:37 -0800

    remove <utility> include

--- include/dango-standard-headers.hpp
+++ include/dango-standard-headers.hpp
@@ -8,6 +8,23 @@
 #include <exception>
 #include <new>
 #include <typeinfo>
+
+#ifdef __GLIBCXX__
+namespace
+std
+{
+  template
+  <typename tp_type>
+  struct tuple_size;
+
+  template
+  <std::size_t tp_index, typename tp_type>
+  struct tuple_element;
+}
+#endif
+
+#ifdef _LIBCPP_VERSION
 #include <utility>
+#endif
 
 #endif // DANGO_STANDARD_HEADERS_HPP_INCLUDED
